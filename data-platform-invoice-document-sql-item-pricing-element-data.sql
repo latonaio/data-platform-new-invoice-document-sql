0a384d37d9408907a82d64e5abc0a1ddfdb427b2
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-06-29 12:19:09 +0900

    release

--- data-platform-invoice-document-sql-item-pricing-element-data.sql
+++ data-platform-invoice-document-sql-item-pricing-element-data.sql
@@ -3,22 +3,32 @@ CREATE TABLE `data_platform_invoice_document_item_pricing_element_data`
   `InvoiceDocument`             int(16) NOT NULL,
   `InvoiceDocumentItem`         int(6) NOT NULL,
   `PricingProcedureCounter`     int(3) NOT NULL,
-  `ConditionRecord`             int(12) DEFAULT NULL,
-  `ConditionSequentialNumber`   int(3) DEFAULT NULL,
-  `ConditionType`               varchar(4) DEFAULT NULL,
-  `PricingDate`                 date DEFAULT NULL,
-  `ConditionRateValue`          float(13) DEFAULT NULL,
-  `ConditionCurrency`           varchar(5) DEFAULT NULL,
-  `ConditionQuantity`           float(6) DEFAULT NULL,
-  `ConditionQuantityUnit`       varchar(3) DEFAULT NULL,
+  `SupplyChainRelationshipID`   int(16) NOT NULL,
+  `Buyer`                       int(12) NOT NULL,
+  `Seller`                      int(12) NOT NULL,
+  `ConditionRecord`             int(12) NOT NULL,
+  `ConditionSequentialNumber`   int(3) NOT NULL,
+  `ConditionType`               varchar(4) NOT NULL,
+  `PricingDate`                 date NOT NULL,
+  `ConditionRateValue`          float(13) NOT NULL,
+  `ConditionRateValueUnit`      int(6) NOT NULL,
+  `ConditionScaleQuantity`      int(13) NOT NULL,
+  `ConditionCurrency`           varchar(5) NOT NULL,
+  `ConditionQuantity`           float(6) NOT NULL,
   `TaxCode`                     varchar(2) DEFAULT NULL,
-  `ConditionAmount`             float(13) DEFAULT NULL,
-  `TransactionCurrency`         varchar(5) DEFAULT NULL,
+  `ConditionAmount`             float(13) NOT NULL,
+  `TransactionCurrency`         varchar(5) NOT NULL,
   `ConditionIsManuallyChanged`  tinyint(1) DEFAULT NULL,
+  `CreationDate`                date NOT NULL,
+  `CreationTime`                time NOT NULL,
+  `LastChangeDate`              date NOT NULL,
+  `LastChangeTime`              time NOT NULL,
+  `IsCancelled`                 tinyint(1) DEFAULT NULL,
   
   PRIMARY KEY (`InvoiceDocument`, `InvoiceDocumentItem`, `PricingProcedureCounter`),
     
-  CONSTRAINT `DataPlatformInvoiceDocumentItemPricingElementData_fk` FOREIGN KEY (`InvoiceDocument`, `InvoiceDocumentItem`) REFERENCES `data_platform_invoice_document_item_data` (`InvoiceDocument`, `InvoiceDocumentItem`)
+  CONSTRAINT `DataPlatformInvoiceDocumentItemPricingElementData_fk` FOREIGN KEY (`InvoiceDocument`, `InvoiceDocumentItem`) REFERENCES `data_platform_invoice_document_item_data` (`InvoiceDocument`, `InvoiceDocumentItem`),
+  CONSTRAINT `DataPlatformInvoiceDocumentItemPricingElementDataSCRID_fk` FOREIGN KEY (`SupplyChainRelationshipID`, `Buyer`, `Seller`) REFERENCES `data_platform_supply_chain_relationship_general_data` (`SupplyChainRelationshipID`, `Buyer`, `Seller`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
